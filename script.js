fc3b508e9a40a7b1aa417edbaf588ce333bd5a63
therealkingcob <rishiashah19@gmail.com>
2024-04-14 21:04:19 -0700

    create functions for displaying books

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function book (title, author, pages, read) {
   function addBookToLibrary(book) {
     myLibrary.push(book);
   }
+
+  function displayBooks() {
+    for(let book of myLibrary) {
+        const newDiv = document.createElement("div");
+        const newContent = document.createTextNode(book.info());
+        newDiv.appendChild(newContent);
+        const element = document.getElementById("page");
+        element.appendChild(newDiv);
+    }
+  }
+
+
+const harryPotter = new book("Harry Potter", "J. K. Rowling", 748, true);
+addBookToLibrary(harryPotter);
+displayBooks();
+
